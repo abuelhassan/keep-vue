f28da6b47b557647087f91225169c909b37a59aa
amr <abuelhassan93@gmail.com>
2024-06-17 00:50:46 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Keep me organized"></HelloWorld>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref} from "vue";
+
+defineProps({
+  msg: String,
+})
+const currentNote = ref('')
+const notes = ref([])
+
+function onclick() {
+  notes.value.push({
+    id: notes.value.length,
+    note: currentNote.value
+  })
+  currentNote.value = ''
+}
+</script>
+
+<template>
+  <h1>{{msg}}</h1>
+  <textarea v-model="currentNote"/>
+  <button @click="onclick">Add</button>
+  <div v-for="obj in notes" :key="obj.id">{{obj.note}}</div>
+</template>
+
+<style scoped>
+
+</style>
