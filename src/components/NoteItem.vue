b865ab7684f459e365ede868863cc148555ac13d
amr <abuelhassan93@gmail.com>
2024-06-22 21:07:00 +0100

    NoteItem styling

--- src/components/NoteItem.vue
+++ src/components/NoteItem.vue
@@ -8,7 +8,11 @@
 
 <style scoped>
   .item {
-    min-width: 50px;
-    margin: 10px;
+    width: 240px;
+    min-height: 40px;
+    margin: 16px;
+    padding: 12px 16px 12px 16px;
+    border: 1px darkkhaki solid;
+    border-radius: 5px;
   }
 </style>
